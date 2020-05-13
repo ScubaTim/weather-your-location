0efc4232edd2779aaf9369c640a470d0b9ce56fd
Tim Gentry <gentry.tim@gmail.com>
2020-05-13 15:10:51 -0400

    relative finalization of styles

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import '../../apis/geolocation';
 import { Row, Col } from 'reactstrap';
-import './App.scss';
+import { getLocation } from './App.scss';
 import WeatherDisplay from '../WeatherDisplay';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <Row className="text-center">
+                <Row>
                     <Col>
                         <WeatherDisplay />
                     </Col>
--- src/components/WeatherDisplay/index.js
+++ src/components/WeatherDisplay/index.js
@@ -5,38 +5,42 @@ import './WeatherDisplay.scss';
 const WeatherDisplay = (props) => {
     return (
         <div className="main-display">
-            <Row>
-                <Col className="mt-4 mb-3">
-                    <h1>Current Weather</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="mx-5 my-2">
-                    <Table >
-                        <thead>
-                            <tr>
-                                <th>Temperature</th>
-                                <th>High</th>
-                                <th>Low</th>
-                                <th>Percipitation</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>90F</td>
-                                <td>9000F</td>
-                                <td>20F</td>
-                                <td>100%</td>
-                            </tr>
-                        </tbody>
-                    </Table>
-                </Col>
-            </Row>
-            <Row className="mx-2">
-                <Col className="mx-4 mb-5">
-                    <Button className="btn mb-1 p-2" color="primary" block >Get Weather</Button>
-                </Col>
-            </Row>
+            <div className="mt-3 mb-4 pb-1 mx-4">
+                <Row>
+                    <Col>
+                        <h1 className="display-font">Current Weather</h1>
+                    </Col>
+                </Row>
+                <Row className="text-center my-3">
+                    <Col>
+                        <h5 className="display-font light-text">It is currently raining outside.</h5>
+                    </Col>
+                </Row>
+                <Row className="mx-2 px-5">
+                    <Col>
+                        <Table size="sm" borderless>
+                            <thead>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td><strong>Temperature:</strong> 90&#176;F</td>
+                                    <td><strong>High:</strong> 90&#176;F</td>
+                                </tr>
+                                <tr>
+                                    <td><strong>Percipitation:</strong> 50%</td>
+                                    <td><strong>Low:</strong> 20&#176;F</td>
+                                </tr>
+                            </tbody>
+                        </Table>
+                    </Col>
+                </Row>
+                <Row className="mx-2 mt-3 mb-2">
+                    <Col>
+                        <Button className="btn p-2" color="primary" block >Get Weather</Button>
+                    </Col>
+                </Row>
+            </div>
+
         </div>
     )
 }
