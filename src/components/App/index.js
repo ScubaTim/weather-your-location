5a7eb168acd5bcec87c1eaccf3f074e9daff51aa
Tim Gentry <gentry.tim@gmail.com>
2020-05-13 12:18:59 -0400

    initial styling done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { Row, Col } from 'reactstrap';
+import './App.scss';
+import WeatherDisplay from '../WeatherDisplay';
 
 class App extends Component {
     render() {
         return (
-            <div>The App</div>
+            <div>
+                <Row className="text-center">
+                    <Col>
+                        <WeatherDisplay />
+                    </Col>
+                </Row>
+            </div>
         )
     }
 };
---
+++ src/components/WeatherDisplay/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './WeatherDisplay.scss';
+
+const WeatherDisplay = (props) => {
+    return (
+        <div className="main-display">
+            <h1>Current Weather</h1>
+            <p>Current Tempurature</p>
+        </div>
+    )
+}
+
+export default WeatherDisplay;
